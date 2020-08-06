4aa0ae7e40669381a3b61237772a3f6b643de1a1
Manish Bharti <mbharti321@gmail.com>
2020-08-06 20:09:39 +0530

    Update awk_execution.sh

--- lab8_awk/awk_execution.sh
+++ lab8_awk/awk_execution.sh
@@ -1,4 +1,4 @@
-echo "Manish BHarti"
+eecho "Manish BHarti"
 echo "1947235"
 echo
 echo
@@ -19,21 +19,42 @@ echo
 echo
 echo "1.3. awk '/0.5/ { print \$1 }' input_file"
 echo
-awk '/0.5/ { print \$1 }' $input_file
+awk '/0.5/ { print $1 }' $input_file
 echo
 echo
-echo "1.4. awk '/^[0-9][0-9]/ { print \$1 }' input_file"
+echo "1.4. awk '/^[0-9]/ { print \$0 }' input_file"
 echo
-awk '/^[0-9][0-9]/ { print \$1 }' $input_file
-echo
-echo
-echo "1.5. awk '$2 ~ /ia/ { print \$1 }' input_file"
-echo
-awk '$2 ~ /ia/ { print \$1 }' $input_file
+awk '/^[0-9]/ { print $0 }' $input_file
 echo
 echo
-echo "1.6. awk '$2 !~ /ia/ { print \$1 }' input_file"
+echo "1.5. awk '\$0 ~ /man/ { print \$0 }' input_file"
 echo
+awk '$0 ~ /man/ { print $0 }' $input_file
+echo
+echo
+echo "1.6. awk '\$0 !~ /man/ { print \$0 }' input_file"
+echo
+awk '$0 !~ /man/ { print $0 }' $input_file
+echo
+echo
+echo "1.7. awk '\$4 > 5000 { print \$0 }' input_file"
+echo
+awk '$4 > 500 { print $0 }' $input_file
+echo
+echo
+echo "1.8. awk '\$4 == 31, \$4 == 33 { print \$0 }' input_file"
+echo
+awk '$4 == 85, $4 == 13000 { print $0 }' $input_file
+echo
+echo "1.9. awk 'BEGIN { print "Start Processing."}; { print \$3 }; END { print "End Processing." }' input_file"
+echo
+awk 'BEGIN { print " Start Processing.\n" }; { print $3 }; END { print "\nEnd Processing." }' $input_file
+echo
+echo "1.10. awk 'BEGIN { FS = &quot;.&quot; } { print \$1 }' input_file"
+echo
+awk 'BEGIN {FS = "." } { print $1}' $input_file
+echo
+
 awk '$2 !~ /ia/ { print \$1 }' $input_file
 echo
 echo
