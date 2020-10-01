9bff428eff7999657b703263346ec15a3ab03508
Manish Bharti <mbharti321@gmail.com>
2020-10-01 09:41:51 +0530

    Update numPattern.sh

--- labTest2/numPattern.sh
+++ labTest2/numPattern.sh
@@ -9,7 +9,6 @@ tempArray=(-1 0 1 3 5)
 #to print first half
 for i in {5..1}
 do
-	#temp=`expr $i - 1`
 	temp=$i
 	while [ $i -ge 0 ]
 	do
@@ -18,11 +17,12 @@ do
 	done
 	echo
 done
+
 echo "-1"
+
 #to print second half
 for i in {1..5}
 do
-        #temp=`expr $i - 1`
         temp=$i
         while [ $i -ge 0 ]
         do
